0dfd1bac3d43794a937d6a8d8c131d77cf03f8b1
Nicole Sims <nssims105@gmail.com>
2020-03-27 01:43:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-end
   
   def initialize(brand)
     @brand = brand
+  end
     
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
-  
+end
 
 #nike = Shoe.new("Nike")
 #nike
